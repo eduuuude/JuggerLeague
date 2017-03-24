e9005dc2d8d11af11dffdbdf9f29c7817a57c258
Eneko Montero <eneko.montero@gmail.com>
2017-03-24 10:20:48 +0100

    Añadidas todas las rutas y controladores (menos streaming)

---
+++ app/routes/show.server.routes.js
@@ -0,0 +1,13 @@
+var show = require('../controllers/show.server.controller');
+module.exports = function(app) {
+
+  app.route('/calendario')
+     .get(show.renderCalendario);
+
+	app.route('/combates')
+     .get(show.renderCombates);
+
+	app.route('/clasificacion')
+     .get(show.renderClasificacion);
+
+};
--- config/express.js
+++ config/express.js
@@ -35,6 +35,7 @@ module.exports = function() {
     
     require('../app/routes/index.server.routes.js')(app);
     require('../app/routes/users.server.routes.js')(app);
+		require('../app/routes/show.server.routes.js')(app);
 
     app.use(express.static('./public'));
 
