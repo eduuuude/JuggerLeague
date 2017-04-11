063eff0e189b8d02bf50b7bb7157af94e32f60e0
Jimena <jimena_cab@hotmail.com>
2017-04-11 17:55:02 +0200

    Añadida la tienda
    Para probarla leer el readme

---
+++ app/models/shop.server.model.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var ShopSchema = new Schema({
+    prid: Number,
+    producto: String,
+    precio: Number,
+    nivel: Number,
+    todos: {}//we will use this in the next tutorial to store TODOs
+});
+
+
+mongoose.model('Shop', ShopSchema);
