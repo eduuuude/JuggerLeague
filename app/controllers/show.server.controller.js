d2414243ab000f89d5090dabeab285579c19f663
Lucía <lucia.conde@outlook.com>
2017-04-26 19:38:16 +0200

    Añadida página de clasificación

--- app/controllers/show.server.controller.js
+++ app/controllers/show.server.controller.js
@@ -39,15 +39,26 @@ exports.renderCombates = function(req, res, next) {
 };
 
 exports.renderClasificacion = function(req, res, next) {
-    if (req.user) {
-        res.render('clasificacion', {
-            title: 'Clasificacion',
-            tipo: req.user ? req.user.tipo : ''
-        });
-    }
-    else {
-        return res.redirect('/');
-    }
+  User.find({}, null, {sort: { puntos: -1 }}, function(err, users) {
+      if (err) {
+          return next(err);
+      }
+      else {
+          //res.json(users);
+          if (req.user) {
+              res.render('clasificacion', {
+                  title: 'Clasificacion',
+                  tipo: req.user ? req.user.tipo : '',
+                  nombre: req.user ? req.user.username : '',
+                  equipo: req.user ? req.user.equipo : '',
+                  "data": users
+              });
+          }
+          else {
+              return res.redirect('/');
+          }
+      }
+  });
 };
 
 exports.renderEnDirecto = function(req, res, next) {
